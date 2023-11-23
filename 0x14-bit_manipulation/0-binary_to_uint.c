033f7f3695442db4dd84d2280bddd8f94b0302df
Aviwe <aviwenkambule@gmail.com>
2023-11-23 06:46:20 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,22 +7,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int ui;
-int len, base_two;
+int j;
+unsigned int dec_val = 0;
 if (!b)
 return (0);
-ui = 0;
-for (len = 0; b[len] != '\0'; len++)
-for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+for (j = 0; b[j]; j++)
 {
-if (b[len] != '0' && b[len] != '1')
-{
+if (b[j] < '0' || b[j] > '1')
 return (0);
+dec_val = 2 * dec_val + (b[j] - '0');
 }
-if (b[len] & 1)
-{
-ui += base_two;
+return (dec_val);
 }
-}
-return (ui);
-}
+
