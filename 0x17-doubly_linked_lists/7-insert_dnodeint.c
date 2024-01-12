0c63eb9fc9fc45c7aecb5d78d7c978e74e4443d9
Aviwe <aviwenkambule@gmail.com>
2024-01-12 02:57:53 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,49 +3,32 @@
 /**
  * insert_dnodeint_at_index - inserts a new node at
  * a given position
- * @head of the list
+ * @h: of the list
  * @idx: index of the new node
  * @n: value of the new node
  * Return: the address of the new node, or NULL if failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new;
-dlistint_t *head;
-unsigned int i;
-new = NULL;
+dlistint_t *tmp = *h, *new;
 if (idx == 0)
-new = add_dnodeint(h, n);
-else
+return (add_dnodeint(h, n));
+for (; idx != 1; idx--)
 {
-head = *h;
-i = 1;
-if (head != NULL)
-while (head->prev != NULL)
-head = head->prev;
-while (head != NULL)
-{
-if (i == idx)
-{
-if (head->next == NULL)
-new = add_dnodeint_end(h, n);
-else
-{
+tmp = tmp->next;
+if (tmp == NULL)
+return (NULL);
+}
+if (tmp->next == NULL)
+return (add_dnodeint(h, n));
 new = malloc(sizeof(dlistint_t));
-if (new != NULL)
-{
+if (new == NULL)
+return (NULL);
+
 new->n = n;
-new->next = head->next
-new->prev = head;
-head->next->prev = new;
-head->next = new;
-}
-}
-break;
-}
-head = head->next;
-i++;
-}
-}
+new->prev = tmp;
+new->prev = tmp->next;
+tmp->next->prev = new;
+tmp->next = new;
 return (new);
 }
