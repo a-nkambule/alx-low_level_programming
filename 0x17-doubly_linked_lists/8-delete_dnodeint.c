9af87f73ac99bdf73faea4a84123748958e75b28
Aviwe <aviwenkambule@gmail.com>
2024-01-12 03:11:30 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,55 +9,27 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *h1;
-dlistint_t *h2;
-unsigned int i;
-
-h1 = *head;
-if (h1 != NULL)
-while (h1->prev != NULL)
-h1 = h1->prev;
-i = 0;
-while (h1 != NULL)
-{
-if (i == index)
-{
-if (i == 0)
-{
-*head = h1->next;
-if (*head != NULL)
-(*head)->prev = NULL;
-}
-else
-{
-h2->next = h1->next;
-h1 = *head;
-if (h1 != NULL)
-while (h1->prev != NULL)
-h1 = h1->prev;
-i = 0;
-while (h1 != NULL)
-{
-if (i == index)
-{
-if (i == 0)
-{
-*head = h1->next
-if (*head != NULL)
-(*head)->prev = NULL;
-}
-else
-{
-h2->next = h1->next;
-if (h1->next != NULL)
-h1->next->prev = h2;
-}
-free(h1);
-return (1);
-}
-h2 = h1;
-h1 = h1->next;
-i++;
-}
+dlistint_t *tmp = *head;
+if (*head == NULL)
 return (-1);
+for (; index != 0; index--)
+{
+if (tmp == NULL)
+return (-1);
+tmp = tmp->next;
+}
+if (tmp == *head)
+{
+*head = tmp->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+}
+else
+{
+tmp->prev->next = tmp->next;
+if (tmp->next != NULL)
+tmp->next->prev = tmp->prev;
+}
+free(tmp);
+return (1);
 }
