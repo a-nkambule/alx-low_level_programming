d29bceb23a56648fcf77ca9a2bc6f0ff9207ecf8
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-02-02 04:25:52 -0800

    0x1A-hash_tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,25 +1,28 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_get - Retrieve the value associated with
- * a key in a hash table
- * @ht: A pointer tovthe hash table
- * @key: the key to get the value of
- * Return: If the key cannot be matched - NULL
- * otherwise - the value associated with key in ht
+ * hash_table_get - function to retrieve value associeted with key
+ * @ht: pointer to hash table
+ * @key: key to retrive value
+ *
+ * Return: value or NULL if key does not exit.
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *node;
-unsigned long int index;
+	unsigned long int index = 0;
+	hash_node_t  *bucket;
 
-if (ht == NULL || KEY == NULL || *key == '\0')
-return (NULL);
-index = key_index((const unsigned char *)key, ht->size);
-if (index >= ht->size)
-return (NULL);
-node = ht-> array[index];
-while (node && strcmp(node->key, key) != 0)
-node = node->next;
-return ((node == NULL) ? NULL : node->value);
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[index];
+
+	while (bucket)
+	{
+		if (!strcmp(key, bucket->key))
+			return (bucket->value);
+		bucket = bucket->next;
+	}
+	return (NULL);
 }
