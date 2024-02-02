7be04a3b692e3de12fa9f3c665b3f230ce135792
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-02-02 04:00:57 -0800

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,23 +3,22 @@
 /**
  * hash_table_create - Creates a hash table
  * @size: the size of the array
- * Return: if an error occurs - NULL
- * otherwise - a pointer to the new hash table
+ * Return: pointer to hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *ht;
-unsigned long int i;
-
-ht = malloc(sizeof(hash_table_t));
-if (ht == NULL)
+hash_table_t *table;
+if (size == 0)
 return (NULL);
-
-ht->size = size;
-ht->array = malloc(sizeof(hash_node_t *) *size);
-if (ht->array == NULL)
-return (NULL)
-for (i = -; i < size; i++)
-ht->array[i] = NULL;
-return (ht);
+table = calloc(1, sizeof(hash_table_t));
+if (table== NULL)
+return (NULL);
+table->size = size;
+table->array = calloc(size, sizeof(hash_node_t*));
+if (table->array == NULL)
+{
+free(table);
+return (NULL);
+}
+return (table);
 }
